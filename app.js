8111fefd271317f757810b11117623bbe91abcf6
kes-phyl <danphylbert@gmail.com>
2022-11-06 21:22:30 +0000

    Connect database store default items once

--- app.js
+++ app.js
@@ -1,18 +1,96 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const newItems = ['Buy Food', 'Cook Food', 'Eat Food'];
 const date = require(__dirname + '/date.js')
 const newWorkActivity = ['learn coding'];
+const mongoose = require('mongoose')
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
-   const day = date.getDate();
-   res.render('lists', {kindOfDay: day, newActivity: newItems});
+
+
+
+//connecting to the database... 
+
+mongoose.connect(
+    'mongodb://127.0.0.1:27017/tasksDB'
+  )
+  .then(()=>console.log('connected'))
+  .catch(e=>console.log(e));
+
+
+// const connectDB = async ()=>{
+//     try{
+//         await mongoose.connect('mongodb://localhost:27017/tasksDB');
+//         console.log('connected successfully..!');
+//     }catch(err){
+//         console.log('failed to connect')
+//     }
+// }
+
+
+
+
+const taskSchema = {
+    name: String
+}
+
+const Task = mongoose.model('task', taskSchema);
+
+
+const firstTask = new Task({
+    name: 'Welcome to the task app'
 })
+// firstTask.save(function(err){
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log('succesffully added..')
+//     }
+// })
+const secondTask = new Task({
+    name: 'Hit the + button to add a new task.'
+})
+const thirdTask = new Task({
+    name: '<--- Hit this to delete an item'
+})
+
+const newTasks = [firstTask, secondTask, thirdTask];
+// Task.insertMany(newTasks, function(err){
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log('saved sucessfully');
+//     }
+// });
+
+
+
+app.get('/', function(req, res){
+
+    const day = date.getDate();
+    Task.find({}, function(err, foundTasks){
+    if(foundTasks.length === 0 ){
+        Task.insertMany(newTasks, function(err){
+          
+    if(err){
+        console.log(err)
+    }else{
+        console.log('saved sucessfully');
+        res.render('lists', {kindOfDay: day, newActivity: foundTasks})
+
+    }
+});
+    }
+    
+            res.render('lists', {kindOfDay: day, newActivity: foundTasks})
+   })
+ })
+
+
+
 
 //creating a post route
 
